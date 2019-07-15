dbc8ec44e76737d3c67d85d20ba7f2b467fd9115
Kirich <ivadik2323@gmail.com>
2019-07-15 19:46:56 +1000

    Start making kanji tooltips

---
+++ Assets/Scripts/KanjiToolTip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KanjiToolTip : MonoBehaviour {
+
+    public string Kanji;
+
+    private Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
